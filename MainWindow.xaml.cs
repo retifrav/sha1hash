4d28170af9dec86b8f9fe2d31178fedc045709a2
retif <retifrav@gmail.com>
2015-09-21 20:06:51 +0300

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace sha1hash
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            hashDat.Focus();
+
+            // when F1 is pressed, show help window
+            CommandBinding helpBinding = new CommandBinding(ApplicationCommands.Help);
+            helpBinding.Executed += f1pressed;
+            CommandBindings.Add(helpBinding);
+        }
+
+        private void f1pressed(object sender, ExecutedRoutedEventArgs e)
+        {
+            HelpWindow hlp = new HelpWindow();
+            hlp.ShowDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            log.Clear();
+
+            string dirName = dir.Text.Trim();
+            if (string.IsNullOrEmpty(dirName))
+            {
+                dirName = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            }
+            if (!Directory.Exists(dirName))
+            {
+                MessageBox.Show(
+                    "По указанному пути нет папки с таким именем.",
+                    "Не найден каталог",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error                    
+                    );
+                return;
+            }
+            
+            try
+            {
+                List<string> files = Directory.GetFiles(
+                    dirName,
+                    "*.*",
+                    SearchOption.TopDirectoryOnly
+                    ).Where(f => f.ToLower().EndsWith("png") || f.ToLower().EndsWith("jpg") || f.ToLower().EndsWith("gif")).ToList<string>();
+                foreach (string file in files)
+                {
+                    using (FileStream stream = File.OpenRead(file))
+                    {
+                        SHA1Managed sha = new SHA1Managed();
+                        byte[] checksum = sha.ComputeHash(stream);
+                        //log.Text += System.IO.Path.GetFileName(file) + " - " + BitConverter.ToString(checksum).Replace("-", string.Empty) + Environment.NewLine;
+                        log.Text += BitConverter.ToString(checksum).Replace("-", string.Empty) + Environment.NewLine;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Не удалось получить SHA1 для файлов по указанному пути. " + ex.Message,
+                    "Ошибка вычисления SHA1",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error                    
+                    );
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+sha1hash
+=========
+    
+Nothing fancy, just getting SHA1 hashes for files in the specified directory with specified extensions (`.jpg`, `.png` and `.gif`). No one will ever use it, so everything is hardcoded.
+
+![sha1hash main window screenshot](/img/mainwindow.png?raw=true "NWShelper main window screenshot")
+
+3rd party
+=========
+
+The application is written in `C#`, `WPF` with `Visual Studio 2013` (if that matters) and `.NET 4.5.1`. Those nasty bastards did all the work, I just wrote a few lines of code.
+
+And I snatched some (all) icons from [Iconfinder](https://www.iconfinder.com/).
